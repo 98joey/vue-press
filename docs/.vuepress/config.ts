a096ed5810a7e32efb7d0aef7205a27f2273d974
jiaming.wu <jiaming.wu@nio.com>
2024-02-20 15:07:59 +0800

    make base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,7 @@ import { defineUserConfig } from "vuepress";
 export default defineUserConfig({
   bundler: viteBundler(),
   theme: defaultTheme(),
+  base: '/vue-press/',
   lang: 'zh-CN',
   title: 'Hello，这是我的第一个 VuePress 站点',
   pagePatterns: ['**/*.md', '!node_modules'],  
